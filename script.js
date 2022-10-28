73f574e70a161e9cbd915219e59e128588945e70
Badra Mohamed <moha1150@umn.edu>
2022-10-27 19:45:02 -0500

    practice js

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('loops practice'); 
+ 
+let index = 0 
+const max = 10
+
+while (index < max){
+    console.log('in while loops', index); 
+    index++;
+    // index++ needs to add the increameant
+}
